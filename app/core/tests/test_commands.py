2fabb96b750bdbc7d29d09c831c42ccc14d9cdf9
Jarema Piekutowski <jarema.piekutowski@gmail.com>
2023-09-24 23:01:28 +0200

    some linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,34 +2,29 @@
 Test custom Django management commands
 """
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-# The patch decorator replaces the behavior of the check() method with a mock
-#  function. The mock function will return True instead of raising an error.
-#  This will speed up the test.
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands"""
 
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for db when db is available"""
+        """Test waiting for db if database ready."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(
-            databases=['default']
-        )
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for database when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
